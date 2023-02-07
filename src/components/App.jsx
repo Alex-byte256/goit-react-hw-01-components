3a4bb2bd516a10b9ddf8eb8642037b1feeb771bd
Alex-byte256 <oleksandr.kozak.2019@gmail.com>
2023-02-07 14:01:27 +0300

    allready-1-task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,7 @@ export const App = () => {
   return (
     <div>
       <Profile stats={user.stats} location={user.location} avatar={user.avatar} tag={user.tag} username={user.username}/>
+
     </div>
   );
 };
